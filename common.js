4898311aad2418fac84f835eac552cdb4b8ef1b3
y2sub <50686452+yesubway@users.noreply.github.com>
2025-04-25 11:09:11 +0900

    common.js update
    blog.html 추가

--- common.js
+++ common.js
@@ -1,5 +1,6 @@
 // common.js
 document.addEventListener("DOMContentLoaded", function () {
+  // nav.html 삽입 (div 안에)
   fetch("nav.html")
     .then(response => response.text())
     .then(data => {
@@ -7,5 +8,14 @@ document.addEventListener("DOMContentLoaded", function () {
     })
     .catch(error => {
       console.error("네비게이션 로딩 실패:", error);
+    });
+  // blog.html 삽입 (main 안에)
+  fetch("blog.html")
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("mainblog").innerHTML = data;
+    })=
+    .catch(error => {
+      console.error("블로그 미리보기 로딩 실패:", error);
     });
 });
